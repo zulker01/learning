9e4f0a5d5ba3faa2caa64c65cb0af75a7bff7d84
zulker01 <zulker02@gmail.com>
2023-12-07 15:14:54 +0600

    init commit

---
+++ demowebflux/demo/src/main/java/Entity/Employee.java
@@ -0,0 +1,20 @@
+package Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(value = "employee_info")
+public class Employee {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+}
---
+++ demowebflux/demo/src/main/java/com/example/webflux/demo/DemowebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebfluxApplication.class, args);
+	}
+
+}
